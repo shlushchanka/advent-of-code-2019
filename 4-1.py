d2be34f3a175960f49d6f70501214468c25ae021
Siarhei Hlushchanka <s.hlushchanka@gmail.com>
2019-12-04 09:41:38 +0100

    day 4 part 1

---
+++ 4-1.py
@@ -0,0 +1,31 @@
+def get_input(): 
+  return (272091, 815432)
+
+def digits(n):
+  if n == 0:
+    return [0]
+  else:
+    digits = []
+    while n > 0:
+      last = n % 10
+      digits.append(last)
+      n /= 10
+    digits = list(reversed(digits))
+    return digits  
+
+def generate_valid_passwords_in_range(start, end):
+  for n in range(start, end + 1):
+    n_digits = digits(n)
+    if is_sorted(n_digits) and has_two_equal_adjacent_digits(n_digits):
+      yield n
+
+def is_sorted(array):
+  return all(array[i] <= array[i + 1] for i in xrange(len(array) - 1))
+
+def has_two_equal_adjacent_digits(array):
+  return any(array[i] == array[i + 1] for i in xrange(len(array) - 1))
+
+if __name__ == '__main__':
+  (start, end) = get_input()
+  count = len(list(generate_valid_passwords_in_range(start, end)))
+  print(count)
