47b80663864ea14b2a1df4c9dc3a5d078d3bf089
Siarhei Hlushchanka <s.hlushchanka@gmail.com>
2019-12-06 09:34:39 +0100

    day 6 part 1

---
+++ 6-1.py
@@ -0,0 +1,34 @@
+def count_orbits(graph):
+  vertex_to_subtree_size = {}
+  for v in graph.keys():
+    if v not in vertex_to_subtree_size:
+      dfs(graph, vertex_to_subtree_size, v)
+  counts = vertex_to_subtree_size.values()
+  return sum(counts)
+
+def dfs(graph, vertex_to_subtree_size, v):
+  if v not in graph:
+    vertex_to_subtree_size[v] = 0
+    return
+  size = len(graph[v])
+  for child in graph[v]:
+    if child not in vertex_to_subtree_size:
+      dfs(graph, vertex_to_subtree_size, child)
+    size += vertex_to_subtree_size[child]
+  vertex_to_subtree_size[v] = size
+
+
+def get_input(): 
+  graph = {}
+  filename = '6.txt'
+  lines = [line.rstrip('\n') for line in open(filename)]
+  for line in lines:
+    (parent, child) = line.split(')')
+    if parent not in graph:
+      graph[parent] = []
+    graph[parent].append(child)
+  return graph
+
+if __name__ == '__main__':
+  graph = get_input()
+  print(count_orbits(graph))
