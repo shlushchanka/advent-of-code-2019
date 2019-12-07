da85c12a2e8a017487771a97f19aec7f87ca7f2c
Siarhei Hlushchanka <shlushchanka@gmail.com>
2019-12-07 21:00:52 +0100

    refactor day 7 part 2

--- 7-2.py
+++ 7-2.py
@@ -3,24 +3,26 @@ from IntCodeComputer import IntCodeComputer
 def get_input():
   return [3,8,1001,8,10,8,105,1,0,0,21,30,39,64,81,102,183,264,345,426,99999,3,9,1001,9,2,9,4,9,99,3,9,1002,9,4,9,4,9,99,3,9,1002,9,5,9,101,2,9,9,102,3,9,9,1001,9,2,9,1002,9,2,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,3,9,4,9,99,3,9,102,4,9,9,1001,9,3,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,99]
 
+def build_computer_ring(program, std_ins):
+  computers = []
+  for i in range(len(std_ins)):
+    memory = program.copy()
+    std_in = std_ins[i]
+    std_out = std_ins[(i + 1) % len(std_ins)]
+    computers.append(IntCodeComputer(memory, std_in, std_out))
+  return computers
+
 def run_circuit(program, phases):
   std_ins = [ [phase] for phase in phases ]
   std_ins[0].append(0)
-  computers = []
-  for i in range(len(phases)):
-    memory = program.copy()
-    std_in = std_ins[i]
-    std_out = std_ins[(i + 1) % len(phases)]
-    computers.append(IntCodeComputer(memory, std_in, std_out))
+  computers = build_computer_ring(program, std_ins)
 
   any_waiting_for_input = True
   while any_waiting_for_input:
     any_waiting_for_input = False
     for c in computers:
-      if c.is_halted:
-        continue
       c.run()
-      any_waiting_for_input = any_waiting_for_input or c.is_waiting_for_input
+      any_waiting_for_input |= c.is_waiting_for_input
       
   return computers[-1].std_out[-1]
 
