bd1aef529c559c1877d5e6bea850b8576a54d5e9
Siarhei Hlushchanka <s.hlushchanka@gmail.com>
2019-12-11 00:45:06 +0100

    refactor 10-1.py

--- 10-1.py
+++ 10-1.py
@@ -1,7 +1,3 @@
-def find_max(field):
-  asteroids = asteroid_locations(field)
-  return max(count(asteroids, station_location) for station_location in asteroids) - 1
-
 def asteroid_locations(field):
   locations = set()
   for row in range(len(field)):
@@ -10,35 +6,24 @@ def asteroid_locations(field):
         locations.add(point(col, row))
   return locations
 
-def count(asteroids, location):
-  vectors = set()
+def count_visible(asteroids, location):
+  angle_set = set()
   for asteroid in asteroids:
-    vector = normalised_vector(source=location, dest=asteroid)
-    vectors.add(str(vector))
-  return len(vectors)
+    if asteroid != location:
+      angle = relative_angle(location, asteroid)
+      angle_set.add(angle)
+  return len(angle_set)
 
-def normalised_vector(source, dest):
-  from fractions import Fraction
-  import math
-  (source_x, source_y) = source
-  (dest_x, dest_y) = dest
-  (delta_x, delta_y) = (dest_x - source_x, dest_y - source_y)
-  if delta_y == 0:
-    ratio = Fraction(0, 1)
-  else:
-    ratio = Fraction(delta_x, delta_y)
-  return (sign(delta_x), sign(delta_y), ratio)
+def relative_angle(source, dest):
+  (x, y) = vector(source, dest)
+  from math import atan2
+  return atan2(y, x)
 
-def sign(n):
-  if n < 0:
-    return -1
-  elif n == 0:
-    return 0
-  else:
-    return 1
+def vector(source, dest):
+  return tuple([d - s for (s, d) in zip(source, dest)])
 
-def point(row, col):
-  return (row, col)
+def point(x, y):
+  return (x, y)
 
 def get_input():
   filename = '10.txt'
@@ -46,4 +31,5 @@ def get_input():
   
 if __name__ == '__main__':
   field = get_input()
-  print(find_max(field))
+  asteroids = asteroid_locations(field)
+  print(max(count_visible(asteroids, station_location) for station_location in asteroids))
